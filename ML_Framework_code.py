9e7521ea344cc2b8abf8c7450c7c88073cad1882
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-06-22 18:08:08 +0300

    Update ML_Framework_code.py

--- ML_Framework_code.py
+++ ML_Framework_code.py
@@ -21,10 +21,37 @@ def get_binary_file_downloader_html(bin_file, file_label="File"):
     href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{bin_file}">Download {file_label}</a>'
     return href
 
+# Define a function to perform EDA on the data
+def perform_eda(data):
+    # Handle missing values
+    if handle_missing_values:
+        data = data.dropna() # drop rows with missing values
+    
+    # Handle outliers using the Z-score method
+    if handle_outliers:
+        z_scores = (data - data.mean()) / data.std()
+        data = data[(z_scores < 3).all(axis=1)]
+    
+    # Normalize the data using min-max scaling
+    if normalize_data:
+        scaler = MinMaxScaler()
+        data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
+    
+    # Encode categorical variables using one-hot encoding
+    if encode_categorical_variables:
+        categorical_columns = data.select_dtypes(include=['object']).columns
+        encoder = OneHotEncoder()
+        encoded_data = encoder.fit_transform(data[categorical_columns])
+        data = pd.concat([data.drop(categorical_columns, axis=1), pd.DataFrame(encoded_data.toarray(), columns=encoder.get_feature_names_out())], axis=1)
+    
+    # Visualize the relationships between the variables using a pairplot
+    if visualize_data:
+        sns.pairplot(data)
+        plt.show()
+
 # Create a title and a sidebar for the app
 st.title("End to eand ML Regression Model Builder")
 st.sidebar.header("User Input")
-
 
 # upload file
 uploaded_file = st.sidebar.file_uploader("Upload your data file", type=["csv", "xlsx"])
@@ -45,35 +72,7 @@ if uploaded_file is not None:
     normalize_data = st.sidebar.checkbox("Normalize data", value=False)
     encode_categorical_variables = st.sidebar.checkbox("Encode categorical variables", value=True)
     visualize_data = st.sidebar.checkbox("Visualize data", value=True)
-    
-    # Define a function to perform EDA on the data
-    def perform_eda(data):
-        # Handle missing values
-        if handle_missing_values:
-            data = data.dropna() # drop rows with missing values
-        
-        # Handle outliers using the Z-score method
-        if handle_outliers:
-            z_scores = (data - data.mean()) / data.std()
-            data = data[(z_scores < 3).all(axis=1)]
-        
-        # Normalize the data using min-max scaling
-        if normalize_data:
-            scaler = MinMaxScaler()
-            data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
-        
-        # Encode categorical variables using one-hot encoding
-        if encode_categorical_variables:
-            categorical_columns = data.select_dtypes(include=['object']).columns
-            encoder = OneHotEncoder()
-            encoded_data = encoder.fit_transform(data[categorical_columns])
-            data = pd.concat([data.drop(categorical_columns, axis=1), pd.DataFrame(encoded_data.toarray(), columns=encoder.get_feature_names_out())], axis=1)
-        
-        # Visualize the relationships between the variables using a pairplot
-        if visualize_data:
-            sns.pairplot(data)
-            plt.show()
-
+   
     # Perform EDA on the data
     perform_eda(data)
 
