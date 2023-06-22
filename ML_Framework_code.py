a68d3078497080aef522d10da18765a7a749e3e6
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-06-22 18:03:16 +0300

    Update ML_Framework_code.py

--- ML_Framework_code.py
+++ ML_Framework_code.py
@@ -47,31 +47,31 @@ if uploaded_file is not None:
     
     # Define a function to perform EDA on the data
     def perform_eda(data):
-    # Handle missing values
-    if handle_missing_values:
-        data = data.dropna() # drop rows with missing values
-    
-    # Handle outliers using the Z-score method
-    if handle_outliers:
-        z_scores = (data - data.mean()) / data.std()
-        data = data[(z_scores < 3).all(axis=1)]
-    
-    # Normalize the data using min-max scaling
-    if normalize_data:
-        scaler = MinMaxScaler()
-        data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
-    
-    # Encode categorical variables using one-hot encoding
-    if encode_categorical_variables:
-        categorical_columns = data.select_dtypes(include=['object']).columns
-        encoder = OneHotEncoder()
-        encoded_data = encoder.fit_transform(data[categorical_columns])
-        data = pd.concat([data.drop(categorical_columns, axis=1), pd.DataFrame(encoded_data.toarray(), columns=encoder.get_feature_names_out())], axis=1)
-    
-    # Visualize the relationships between the variables using a pairplot
-    if visualize_data:
-        sns.pairplot(data)
-        plt.show()
+        # Handle missing values
+        if handle_missing_values:
+            data = data.dropna() # drop rows with missing values
+        
+        # Handle outliers using the Z-score method
+        if handle_outliers:
+            z_scores = (data - data.mean()) / data.std()
+            data = data[(z_scores < 3).all(axis=1)]
+        
+        # Normalize the data using min-max scaling
+        if normalize_data:
+            scaler = MinMaxScaler()
+            data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
+        
+        # Encode categorical variables using one-hot encoding
+        if encode_categorical_variables:
+            categorical_columns = data.select_dtypes(include=['object']).columns
+            encoder = OneHotEncoder()
+            encoded_data = encoder.fit_transform(data[categorical_columns])
+            data = pd.concat([data.drop(categorical_columns, axis=1), pd.DataFrame(encoded_data.toarray(), columns=encoder.get_feature_names_out())], axis=1)
+        
+        # Visualize the relationships between the variables using a pairplot
+        if visualize_data:
+            sns.pairplot(data)
+            plt.show()
 
     # Perform EDA on the data
     perform_eda(data)
