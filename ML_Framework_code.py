f573b8ff7b9378e5c208a51e3e6d15daa80db13a
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-06-22 18:23:33 +0300

    Update ML_Framework_code.py

--- ML_Framework_code.py
+++ ML_Framework_code.py
@@ -23,31 +23,34 @@ def get_binary_file_downloader_html(bin_file, file_label="File"):
 
 # Define a function to perform EDA on the data
 def perform_eda(data):
-    # Handle missing values
-    if handle_missing_values:
-        data = data.dropna() # drop rows with missing values
-    
-    # Handle outliers using the Z-score method
-    if handle_outliers:
-        z_scores = (data - data.mean()) / data.std()
-        data = data[(z_scores < 3).all(axis=1)]
-    
-    # Normalize the data using min-max scaling
-    if normalize_data:
-        scaler = MinMaxScaler()
-        data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
-    
-    # Encode categorical variables using one-hot encoding
-    if encode_categorical_variables:
-        categorical_columns = data.select_dtypes(include=['object']).columns
-        encoder = OneHotEncoder()
-        encoded_data = encoder.fit_transform(data[categorical_columns])
-        data = pd.concat([data.drop(categorical_columns, axis=1), pd.DataFrame(encoded_data.toarray(), columns=encoder.get_feature_names_out())], axis=1)
-    
-    # Visualize the relationships between the variables using a pairplot
-    if visualize_data:
-        sns.pairplot(data)
-        st.pyplot()
+     with st.spinner('Performing EDA...'):
+        # Handle missing values
+        if handle_missing_values:
+            data = data.dropna() # drop rows with missing values
+        
+        # Handle outliers using the Z-score method
+        if handle_outliers:
+            z_scores = (data - data.mean()) / data.std()
+            data = data[(z_scores < 3).all(axis=1)]
+        
+        # Normalize the data using min-max scaling
+        if normalize_data:
+            scaler = MinMaxScaler()
+            data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
+        
+        # Encode categorical variables using one-hot encoding
+        if encode_categorical_variables:
+            categorical_columns = data.select_dtypes(include=['object']).columns
+            encoder = OneHotEncoder()
+            encoded_data = encoder.fit_transform(data[categorical_columns])
+            data = pd.concat([data.drop(categorical_columns, axis=1), pd.DataFrame(encoded_data.toarray(), columns=encoder.get_feature_names_out())], axis=1)
+        
+        # Visualize the relationships between the variables using a pairplot
+        if visualize_data:
+            fig, ax = plt.subplots()
+            ax = sns.pairplot(data)
+            st.pyplot(fig)
+        
 
 # Create a title and a sidebar for the app
 st.title("End to end ML Regression Model Builder")
