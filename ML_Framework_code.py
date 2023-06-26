3211174e3aab5b49356322f950fdb4c4f84798a6
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-06-26 16:44:33 +0300

    Update ML_Framework_code.py

--- ML_Framework_code.py
+++ ML_Framework_code.py
@@ -13,7 +13,43 @@ from sklearn.linear_model import ElasticNet, BayesianRidge
 from sklearn.model_selection import RandomizedSearchCV, cross_val_predict, train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import pickle
-import functions
+# import functions
+
+# Helper function to create a download link for a file
+def get_binary_file_downloader_html(bin_file, file_label="File"):
+    import base64
+    bin_str = base64.b64encode(bin_file.encode()).decode()
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{bin_file}">Download {file_label}</a>'
+    return href
+     
+def perform_eda(data):
+     with st.spinner('Performing EDA...'):
+        # Handle missing values
+        if handle_missing_values:
+            data = data.dropna() # drop rows with missing values
+        
+        # Handle outliers using the Z-score method
+        if handle_outliers:
+            z_scores = (data - data.mean()) / data.std()
+            data = data[(z_scores < 3).all(axis=1)]
+        
+        # Normalize the data using min-max scaling
+        if normalize_data:
+            scaler = MinMaxScaler()
+            data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
+        
+        # Encode categorical variables using one-hot encoding
+        if encode_categorical_variables:
+            categorical_columns = data.select_dtypes(include=['object']).columns
+            encoder = OneHotEncoder()
+            encoded_data = encoder.fit_transform(data[categorical_columns])
+            data = pd.concat([data.drop(categorical_columns, axis=1), pd.DataFrame(encoded_data.toarray(), columns=encoder.get_feature_names_out())], axis=1)
+        
+        # Visualize the relationships between the variables using a pairplot
+        #if visualize_data:
+         #   fig = plt.figure()
+          #  sns.pairplot(data)
+           # st.pyplot(fig)
 
 # Create a title and a sidebar for the app
 st.title("End to end ML Regression Model Builder")
@@ -39,7 +75,7 @@ if uploaded_file is not None:
         #visualize_data = st.checkbox("Visualize data", value=True)
        
         # Perform EDA on the data
-        functions.perform_eda(data)
+        perform_eda(data)
     with st.expander("Find best regression model"):
         # Allow the user to select a target column and a split percentage from the sidebar
         features = st.multiselect("Select features columns", data.columns.tolist(), default=data.columns.tolist())
@@ -149,7 +185,7 @@ if uploaded_file is not None:
             st.subheader("Download Best Model")
             st.markdown("Click the button below to download the best model as a pickle file.")
             if st.button("Download"):
-                st.markdown(functions.get_binary_file_downloader_html("best_model.pkl", "Best Model"), unsafe_allow_html=True)
+                st.markdown(get_binary_file_downloader_html("best_model.pkl", "Best Model"), unsafe_allow_html=True)
     
 
 
