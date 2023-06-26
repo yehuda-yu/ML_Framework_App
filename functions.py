a194e056f37456c299511df0afff4b8dfb20fa11
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-06-26 16:38:00 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,9 +6,13 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder, LabelEncoder
 
 
-
-
-# Define a function to perform EDA on the data
+# Helper function to create a download link for a file
+def get_binary_file_downloader_html(bin_file, file_label="File"):
+    import base64
+    bin_str = base64.b64encode(bin_file.encode()).decode()
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{bin_file}">Download {file_label}</a>'
+    return href
+     
 def perform_eda(data):
      with st.spinner('Performing EDA...'):
         # Handle missing values
