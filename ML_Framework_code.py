30aa78faa03aa42533c28b08e772b1548ea7a2c0
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-06-26 16:03:37 +0300

    Update ML_Framework_code.py

--- ML_Framework_code.py
+++ ML_Framework_code.py
@@ -14,51 +14,12 @@ from sklearn.model_selection import RandomizedSearchCV, cross_val_predict, train
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import pickle
 
-# Helper function to create a download link for a file
-def get_binary_file_downloader_html(bin_file, file_label="File"):
-    import base64
-    bin_str = base64.b64encode(bin_file.encode()).decode()
-    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{bin_file}">Download {file_label}</a>'
-    return href
-
-# Define a function to perform EDA on the data
-def perform_eda(data):
-     with st.spinner('Performing EDA...'):
-        # Handle missing values
-        if handle_missing_values:
-            data = data.dropna() # drop rows with missing values
-        
-        # Handle outliers using the Z-score method
-        if handle_outliers:
-            z_scores = (data - data.mean()) / data.std()
-            data = data[(z_scores < 3).all(axis=1)]
-        
-        # Normalize the data using min-max scaling
-        if normalize_data:
-            scaler = MinMaxScaler()
-            data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
-        
-        # Encode categorical variables using one-hot encoding
-        if encode_categorical_variables:
-            categorical_columns = data.select_dtypes(include=['object']).columns
-            encoder = OneHotEncoder()
-            encoded_data = encoder.fit_transform(data[categorical_columns])
-            data = pd.concat([data.drop(categorical_columns, axis=1), pd.DataFrame(encoded_data.toarray(), columns=encoder.get_feature_names_out())], axis=1)
-        
-        # Visualize the relationships between the variables using a pairplot
-        #if visualize_data:
-         #   fig = plt.figure()
-          #  sns.pairplot(data)
-           # st.pyplot(fig)
-        
-
 # Create a title and a sidebar for the app
 st.title("End to end ML Regression Model Builder")
 st.header("User Input")
 
 # upload file
 uploaded_file = st.file_uploader("Upload your data file", type=["csv", "xlsx"])
-
 
 # Perform EDA on the data after it is uploaded and before the model is executed
 if uploaded_file is not None:
