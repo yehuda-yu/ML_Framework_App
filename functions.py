c981f21ac357be3b7ef1a732e70fb7eb144e3eb7
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-07-25 17:50:54 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -4,15 +4,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder, LabelEncoder
-
-
-# Helper function to create a download link for a file
-def get_binary_file_downloader_html(bin_file, file_label="File"):
-    import base64
-    bin_str = base64.b64encode(bin_file.encode()).decode()
-    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{bin_file}">Download {file_label}</a>'
-    return href
-     
+  
 def perform_eda(data,handle_missing_values,handle_outliers,normalize_data,encode_categorical_variables):
      with st.spinner('Performing EDA...'):
         # Handle missing values
@@ -36,6 +28,73 @@ def perform_eda(data,handle_missing_values,handle_outliers,normalize_data,encode
             encoded_data = encoder.fit_transform(data[categorical_columns])
             data = pd.concat([data.drop(categorical_columns, axis=1), pd.DataFrame(encoded_data.toarray(), columns=encoder.get_feature_names_out())], axis=1)
 
+def train_models(models, param_grids, X_train, y_train):
+    best_models = {}  # Store the best models for each type
+    best_scores = {}  # Store the best scores for each type
+    best_params = {}  # Store the best parameters for each type
+
+    for model_type in models.keys():
+        print(f"Training {model_type} model...")
+        # Perform the randomized search with cross-validation
+        search = RandomizedSearchCV(models[model_type], param_grids[model_type], cv=3, n_iter=10, random_state=42)
+        search.fit(X_train, y_train)
+
+        # Store the best model, score, and parameters
+        best_models[model_type] = search.best_estimator_
+        best_scores[model_type] = round(search.best_score_, 2)
+        best_params[model_type] = search.best_params_
+
+    return best_models, best_scores, best_params
+def evaluate_models(best_models, X_test, y_test):
+    # Create a DataFrame to store the evaluation results
+    results = pd.DataFrame(columns=["Model", "MAE", "MSE", "RMSE", "R2", "RPD"])
+
+    # Evaluate each model on the test set and store the results in a dictionary
+    model_evaluations = {}
+    for model_type, model in best_models.items():
+        # Predict the target variable for the test set
+        y_test_pred = model.predict(X_test)
+
+        # Calculate MAE, MSE, RMSE, R2, etc.
+        mae = mean_absolute_error(y_test, y_test_pred)
+        mse = mean_squared_error(y_test, y_test_pred)
+        rmse = np.sqrt(mse)
+        r2 = r2_score(y_test, y_test_pred)
+        rpd = y_test.std() / rmse
+
+        # Append the results to the dataframe
+        results = pd.concat([results, pd.DataFrame({"Model": [model_type],
+                                                    "MAE": [mae],
+                                                    "MSE": [mse],
+                                                    "RMSE": [rmse],
+                                                    "R2": [r2],
+                                                    "RPD": [rpd]})])
+
+        # Store the model evaluation results in the dictionary
+        model_evaluations[model_type] = {
+            "y_test": y_test,
+            "y_test_pred": y_test_pred
+        }
+
+    return results, model_evaluations
+
+def plot_scatter_subplots(model_evaluations):
+    fig, axes = plt.subplots(1, 3, figsize=(18, 9))
+    
+    for i, (model_type, evaluation) in enumerate(model_evaluations.items()):
+        ax = axes[i]
+        ax.scatter(evaluation["y_test_pred"], evaluation["y_test"], alpha=0.8, color='#2a9d8f', edgecolors='black')
+        ax.plot([evaluation["y_test"].min(), evaluation["y_test"].max()],
+                [evaluation["y_test"].min(), evaluation["y_test"].max()],
+                'k--', lw=2)
+        ax.set_xlabel('Predictions (y_test_pred)')
+        ax.set_ylabel('True Values (y_test)')
+        ax.set_title(model_type)
+
+    plt.tight_layout()
+    plt.show()
+    st.pyplot(fig)
+'''
 # Plot prediction vs true test set
 def plot_scatter_subplots(y_test_dict, y_test_pred_dict, model_names):
     """
@@ -67,3 +126,4 @@ def plot_scatter_subplots(y_test_dict, y_test_pred_dict, model_names):
          #   fig = plt.figure()
           #  sns.pairplot(data)
            # st.pyplot(fig)
+'''
