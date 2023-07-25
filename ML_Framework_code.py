348e981214043832b089d2172754b96882c4b63a
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-07-25 14:14:15 +0300

    Update ML_Framework_code.py

--- ML_Framework_code.py
+++ ML_Framework_code.py
@@ -17,145 +17,134 @@ import pickle
 # Helper function to create a download link for a file
 # (Put the implementation of the helper function here)
 
-# Create a title and a sidebar for the app
+# Create a title for the app
 st.title("End to End ML Regression Model Builder")
-st.header("User Input")
 
-# Sidebar Navigation
-section = st.sidebar.radio("Navigation", ["Upload Data", "Exploratory Data Analysis", "Model Training"])
+# Step 1: Upload Data
+st.header("Step 1: Upload Data")
+uploaded_file = st.file_uploader("Upload your data file", type=["csv", "xlsx"])
 
-# Section 1: Upload Data
-if section == "Upload Data":
-    uploaded_file = st.file_uploader("Upload your data file", type=["csv", "xlsx"])
+# Perform EDA on the data after it is uploaded and before the model is executed
+if uploaded_file is not None:
+    try:
+        data = pd.read_csv(uploaded_file)
+    except Exception as e:
+        print(e)
+        data = pd.read_excel(uploaded_file)
 
-    # Perform EDA on the data after it is uploaded and before the model is executed
-    if uploaded_file is not None:
-        try:
-            data = pd.read_csv(uploaded_file)
-        except Exception as e:
-            print(e)
-            data = pd.read_excel(uploaded_file)
+    # Inform the user about successful data upload
+    st.success("Data uploaded successfully!")
 
-        # Inform the user about successful data upload
-        st.success("Data uploaded successfully!")
+    # Step 2: Exploratory Data Analysis (EDA) Options
+    st.header("Step 2: Exploratory Data Analysis (EDA) Options")
+    handle_missing_values = st.checkbox("Handle missing values by drop Nan", value=False)
+    handle_outliers = st.checkbox("Handle outliers by Z-score standardization", value=False)
+    normalize_data = st.checkbox("Normalize data", value=False)
+    encode_categorical_variables = st.checkbox("Encode categorical variables", value=False)
 
-# Section 2: Exploratory Data Analysis
-elif section == "Exploratory Data Analysis":
-    if 'data' in locals():
-        # EDA Options
-        with st.expander("EDA Options"):
-            st.subheader("Exploratory Data Analysis Options")
-            handle_missing_values = st.checkbox("Handle missing values by drop Nan", value=False)
-            handle_outliers = st.checkbox("Handle outliers by Z-score standardization", value=False)
-            normalize_data = st.checkbox("Normalize data", value=False)
-            encode_categorical_variables = st.checkbox("Encode categorical variables", value=False)
+    functions.perform_eda(data, handle_missing_values, handle_outliers, normalize_data, encode_categorical_variables)
 
-            functions.perform_eda(data, handle_missing_values, handle_outliers, normalize_data, encode_categorical_variables)
+    # Step 3: Find best regression model
+    st.header("Step 3: Find best regression model")
+    features = st.multiselect("Select features columns", data.columns.tolist(), default=data.columns.tolist())
+    target_column = st.selectbox("Select the target column", data.columns)
+    data = data[features + [target_column]]
+    split_percentage = st.slider("Select the train-test split percentage", 0.1, 0.9, 0.7)
+    run_model = st.button("Run Model")
 
-# Section 3: Model Training
-elif section == "Model Training":
-    if 'data' in locals():
-        # Find best regression model
-        with st.expander("Model Training"):
-            features = st.multiselect("Select features columns", data.columns.tolist(), default=data.columns.tolist())
-            target_column = st.selectbox("Select the target column", data.columns)
-            data = data[features + [target_column]]
-            split_percentage = st.slider("Select the train-test split percentage", 0.1, 0.9, 0.7)
-            run_model = st.button("Run Model")
+    if run_model:
+        # Present data
+        st.subheader("Data Preview")
+        st.write(data.head())
 
-            if run_model:
-                # Present data
-                st.subheader("Data Preview")
-                st.write(data.head())
+        # Shuffle the data and split it into train and test sets based on the user input
+        data = data.sample(frac=1, random_state=42)  # Shuffle the data
+        X = data.drop(target_column, axis=1)  # Features
+        y = data[target_column]  # Target
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 - split_percentage, random_state=42)
 
-                # Shuffle the data and split it into train and test sets based on the user input
-                data = data.sample(frac=1, random_state=42)  # Shuffle the data
-                X = data.drop(target_column, axis=1)  # Features
-                y = data[target_column]  # Target
-                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 - split_percentage, random_state=42)
+        # Create regression models
+        models = {
+            "Linear Regression": LinearRegression(),
+            "Random Forest": RandomForestRegressor(),
+            "SVM Regression": SVR()
+        }
 
-                # Create regression models
-                models = {
-                    "Linear Regression": LinearRegression(),
-                    "Random Forest": RandomForestRegressor(),
-                    "SVM Regression": SVR()
-                }
+        # Hyperparameter grids for RandomizedSearchCV
+        param_grids = {
+            "Linear Regression": {},  # No hyperparameters to tune for Linear Regression
+            "Random Forest": {
+                'n_estimators': [int(x) for x in np.linspace(start=200, stop=2000, num=10)],
+                'max_features': ['auto', 'sqrt'],
+                'max_depth': [int(x) for x in np.linspace(10, 110, num=11)],
+                'min_samples_split': [2, 5, 10],
+                'min_samples_leaf': [1, 2, 4],
+                'bootstrap': [True, False]
+            },
+            "SVM Regression": {},
+        }
 
-                # Hyperparameter grids for RandomizedSearchCV
-                param_grids = {
-                    "Linear Regression": {},  # No hyperparameters to tune for Linear Regression
-                    "Random Forest": {
-                        'n_estimators': [int(x) for x in np.linspace(start=200, stop=2000, num=10)],
-                        'max_features': ['auto', 'sqrt'],
-                        'max_depth': [int(x) for x in np.linspace(10, 110, num=11)],
-                        'min_samples_split': [2, 5, 10],
-                        'min_samples_leaf': [1, 2, 4],
-                        'bootstrap': [True, False]
-                    },
-                    "SVM Regression": {},
-                }
+        # Model Training
+        st.header("Step 4: Model Training")
+        best_models = {}  # Store the best models for each type
+        best_scores = {}  # Store the best scores for each type
+        best_params = {}  # Store the best parameters for each type
 
-                # Model Training
-                st.subheader("Model Training")
-                best_models = {}  # Store the best models for each type
-                best_scores = {}  # Store the best scores for each type
-                best_params = {}  # Store the best parameters for each type
+        for model_type in models.keys():
+            st.write(f"Training {model_type} model...")
+            with st.spinner('Training in progress...'):
+                # Perform the randomized search with cross-validation
+                search = RandomizedSearchCV(models[model_type], param_grids[model_type], cv=3, n_iter=10, random_state=42)
+                search.fit(X_train, y_train)
 
-                for model_type in models.keys():
-                    st.write(f"Training {model_type} model...")
-                    with st.spinner('Training in progress...'):
-                        # Perform the randomized search with cross-validation
-                        search = RandomizedSearchCV(models[model_type], param_grids[model_type], cv=3, n_iter=10, random_state=42)
-                        search.fit(X_train, y_train)
+            # Print the best parameters and score
+            st.write(f"Best score for {model_type} on the train set: ", round(search.best_score_, 2))
 
-                    # Print the best parameters and score
-                    st.write(f"Best score for {model_type} on the train set: ", round(search.best_score_, 2))
+            # Store the best model, score, and parameters
+            best_models[model_type] = search.best_estimator_
+            best_scores[model_type] = search.best_score_
+            best_params[model_type] = search.best_params()
 
-                    # Store the best model, score, and parameters
-                    best_models[model_type] = search.best_estimator_
-                    best_scores[model_type] = search.best_score_
-                    best_params[model_type] = search.best_params()
+        # Model Evaluation
+        st.header("Step 5: Model Evaluation")
+        # Create a DataFrame to store the evaluation results
+        results = pd.DataFrame(columns=["Model", "MAE", "MSE", "RMSE", "R2", "RPD"])
 
-                # Model Evaluation
-                st.subheader("Model Evaluation")
-                # Create a DataFrame to store the evaluation results
-                results = pd.DataFrame(columns=["Model", "MAE", "MSE", "RMSE", "R2", "RPD"])
+        # Evaluate each model on the test set
+        for model_type in best_models.keys():
+            st.write(f"Evaluating {model_type} model...")
+            # Predict the target variable for the test set
+            y_test_pred = best_models[model_type].predict(X_test)
 
-                # Evaluate each model on the test set
-                for model_type in best_models.keys():
-                    st.write(f"Evaluating {model_type} model...")
-                    # Predict the target variable for the test set
-                    y_test_pred = best_models[model_type].predict(X_test)
+            # Calculate MAE, MSE, RMSE, R2, etc.
+            mae = mean_absolute_error(y_test, y_test_pred)
+            mse = mean_squared_error(y_test, y_test_pred)
+            rmse = np.sqrt(mse)
+            r2 = r2_score(y_test, y_test_pred)
+            rpd = y_test.std() / rmse
 
-                    # Calculate MAE, MSE, RMSE, R2, etc.
-                    mae = mean_absolute_error(y_test, y_test_pred)
-                    mse = mean_squared_error(y_test, y_test_pred)
-                    rmse = np.sqrt(mse)
-                    r2 = r2_score(y_test, y_test_pred)
-                    rpd = y_test.std() / rmse
+            # Append the results to the dataframe
+            results = pd.concat([results, pd.DataFrame({"Model": [model_type],
+                                                        "MAE": [mae],
+                                                        "MSE": [mse],
+                                                        "RMSE": [rmse],
+                                                        "R2": [r2],
+                                                        "RPD": [rpd]})])
 
-                    # Append the results to the dataframe
-                    results = pd.concat([results, pd.DataFrame({"Model": [model_type],
-                                                                "MAE": [mae],
-                                                                "MSE": [mse],
-                                                                "RMSE": [rmse],
-                                                                "R2": [r2],
-                                                                "RPD": [rpd]})])
+        # Display the results as a table
+        st.write(results)
 
-                # Display the results as a table
-                st.write(results)
+        # Download Best Model
+        st.header("Step 6: Download Best Model")
+        selected_model = st.selectbox("Select the model to download", results["Model"])
+        best_model = best_models[selected_model]
+        file_name = f"best_model_{selected_model}.pkl"
 
-                # Download Best Model
-                st.subheader("Download Best Model")
-                selected_model = st.selectbox("Select the model to download", results["Model"])
-                best_model = best_models[selected_model]
-                file_name = f"best_model_{selected_model}.pkl"
+        # Save the selected model as a pickle file
+        with open(file_name, "wb") as f:
+            pickle.dump(best_model, f)
 
-                # Save the selected model as a pickle file
-                with open(file_name, "wb") as f:
-                    pickle.dump(best_model, f)
-
-                # Allow the user to download the selected pickle file with a button
-                st.markdown(f"Click the button below to download the {selected_model} model as a pickle file.")
-                if st.button("Download"):
-                    st.markdown(functions.get_binary_file_downloader_html(file_name, "Best Model"), unsafe_allow_html=True)
+        # Allow the user to download the selected pickle file with a button
+        st.write(f"Click the button below to download the {selected_model} model as a pickle file.")
+        if st.button("Download"):
+            st.markdown(functions.get_binary_file_downloader_html(file_name, "Best Model"), unsafe_allow_html=True)
