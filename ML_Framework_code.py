ce060b72754a20ecb32e1db243acec02e47b3db2
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-07-25 17:35:10 +0300

    Update ML_Framework_code.py

--- ML_Framework_code.py
+++ ML_Framework_code.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams['font.size'] = 1.5
 import functions  # Custom functions.py file
 
 # Machine Learning Libraries
