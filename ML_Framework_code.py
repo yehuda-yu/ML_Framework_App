15dd150a3c4012b5fb8ba86cdbca600e9e3f21ef
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-07-31 14:52:47 +0300

    Update ML_Framework_code.py

--- ML_Framework_code.py
+++ ML_Framework_code.py
@@ -21,6 +21,7 @@ from scipy.stats import expon, reciprocal
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import RandomizedSearchCV, cross_val_predict, train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.inspection import permutation_importance
 import pickle
 
 # Create a title for the app
